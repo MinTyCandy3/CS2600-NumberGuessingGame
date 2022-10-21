a95d2d31784d16e31944a51b9458e0278034782b
Minh Tran <mdtran@cpp.edu>
2022-10-21 16:00:12 -0700

    Added numberGameDemo.c main file, provided outline

---
+++ numberGameDemo.c
@@ -0,0 +1,70 @@
+
+// code main here
+    
+    // Variables here
+
+
+    // Ask for user input for options 1-3
+
+        /*
+
+        example output:
+        ---------------------------------------
+        Press 1 to play a game
+        Press 2 to change the max number
+        Press 3 to quit
+        ---------------------------------------
+        */
+
+    
+
+    /* option 1
+    - Randomize a number from 1 - [10 or user's chosen max number] 
+    - Ask for user input
+    - Repeat until they win or enter 'q'
+    */
+
+        /*
+
+        test case: number is 7
+
+        example output:
+        ---------------------------------------
+        Guess a number between 1-10: 5
+        Higher!
+
+        Guess a number between 1-10: 11
+        INVALID INPUT: Number is not within the bounds!
+
+        Guess a number between 1-10: 7
+        Good job! The number was 7!
+        ---------------------------------------
+        [go back to menu output]
+
+        */
+
+
+    
+
+    /* option 2
+    - Allow user to change the maximum number
+    - the maxiumum number the user can pick will be 20 (cannot be <= 0)
+    *** - in a separate branch -> save_user_max_number, save the user's choice of max num
+    */
+
+        /*
+
+        example output:
+        ---------------------------------------
+        What is the maximum number you would like to pick? (note: Max number is 20!)
+        19
+
+        Okay! The range is now 1 - 19
+        ---------------------------------------
+        [go back to menu output]
+        
+        */
+
+   /* option 3
+   - Display nice message then end program with EXIT SUCCESS
+   */
